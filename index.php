f6bf0f7de9d69c959b73cdfd9d5c2c30c2cb3742
Mucht <Mucht@MacBook-Pro-de-Mathieu.local>
2016-03-03 09:25:21 +0100

    ListOfBooks
    Just listed the books from our data base.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $dbConfig = parse_ini_file( 'db.ini' );
 //var_dump($dbConfig); // Pour voir ce qu'il y a dedans.
 
 try{
-    $dsn = sprintf( 'mysql:host=%s;dbname=%s', $dbConfig[ 'host' ], $dbConfig[ 'dbname' ] ); //on remplace par des joker (%s) grace à sprintf on remplace les %s par ce qu'on veut.
+    $dsn = sprintf( '%s:host=%s;dbname=%s',$dbConfig[ 'driver' ], $dbConfig[ 'host' ], $dbConfig[ 'dbname' ] ); //on remplace par des joker (%s) grace à sprintf on remplace les %s par ce qu'on veut.
     $cn = new PDO( $dsn, $dbConfig[ 'username' ], $dbConfig[ 'password' ] );
     $cn -> query( 'SET CHARCTER SET UTF8' );
     $cn -> query( 'SET NAMES UTF8' );
@@ -16,3 +16,14 @@ try{
 }
 // maintenant la page est connecté avec la BDD.
 // probleme on peut voir nos mdp si on les met sur github... On va créer un db.ini.
+
+
+$booksStmnt = 'SELECT * FROM books';
+$pdoStmnt = $cn -> query( $booksStmnt );
+// var_dump( $pdoStmnt );
+$books = $pdoStmnt -> fetchAll(); // va rechercher les données dans la BDD et les stock sous forme de tableau dans $books.
+// var_dump( $books );
+
+foreach ( $books as $book ) {
+    echo $book[ 'title' ]. '<br>';
+}
