cdd07741ec33ba55e472eebb479d913f4d41d30c
Mucht <Mucht@MacBook-Pro-de-Mathieu.local>
2016-03-10 13:25:46 +0100

    BugFixed
    Fixed the bug I had with the Authors list after class

--- models/Model.php
+++ models/Model.php
@@ -3,23 +3,23 @@
 class Model
 {
     protected $table = '';
-    protecter $cn = null;
+    protected $cn = '';
 
-    public function getRaws() {
+    public function getRows() {
         $sql = 'SELECT * FROM ' . $this -> table; // -- je défini une requete SQL
-        $pdoStmnt = $GLOBALS[ 'cn' ] -> query( $sql ); // -- j'exécute la requete SDL - GLOBALS nous permet de récuperer n'importe quel variable qui n'a pas été définie dans la function.
-        // var_dump( $pdoStmnt );
-        // $books = $pdoStmnt -> fetchAll(); // va rechercher les données dans la BDD et les stock sous forme de tableau dans $books.
+        $stmnt = $GLOBALS[ 'cn' ] -> query( $sql ); // -- j'exécute la requete SDL - GLOBALS nous permet de récuperer n'importe quel variable qui n'a pas été définie dans la function.
+        // var_dump( $stmnt );
+        // $books = $stmnt -> fetchAll(); // va rechercher les données dans la BDD et les stock sous forme de tableau dans $books.
         // var_dump( $books );
 
-        return $pdoStmnt -> fetchAll(); // -- le fetchAll nous fait un tableau d'objet
+        return $stmnt -> fetchAll(); // -- le fetchAll nous fait un tableau d'objet
     }
 
-    public function getRaw( $id ) {
-        $Sql = 'SELECT * FROM ' . $this -> table . ' WHERE id = :id';
-        $Stmnt = $GLOBALS[ 'cn' ] -> prepare( $sql );
-        $Stmnt -> execute( [ 'id' => $id ] );
+    public function getRow( $id ) {
+        $sql = 'SELECT * FROM ' . $this -> table . ' WHERE id = :id';
+        $stmnt = $GLOBALS[ 'cn' ] -> prepare( $sql );
+        $stmnt -> execute( [ 'id' => $id ] );
 
-        return $Stmnt -> fetch(); // je vais chercher le résultat de la requete.
+        return $stmnt -> fetch(); // je vais chercher le résultat de la requete.
     }
 }
--- models/authors.php
+++ models/authors.php
@@ -1,6 +1,6 @@
 <?php
 
-class Authors
+class Authors extends Model
 {
     protected $table = 'authors';
 }
